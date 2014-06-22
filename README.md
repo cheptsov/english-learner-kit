60751823968b24a9419563b925c9d9b523229203
cheptsov <andrey.cheptsov@jetbrains.com>
2014-06-22 12:45:37 +0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+English Learner Kit
+===================
+
+This is a Chrome extension that helps learn phonetic transcription and pronunciation.
+
+To see transcription for a word on the page, move your mouse over it and hold the Ctrl (Cmd for OS X) key. Hold the mouse longer to listen the pronunciation.
+
+To install the extension go to https://chrome.google.com/webstore/detail/english-learner-kit/afmaganbgpebidpiahknfcgbhegckaao
+
+## Roadmap
+
+- Add/remove the word to the favorites
+- Show if the word is in favorites
---
+++ myscript.js
@@ -0,0 +1,177 @@
+function spanify(paragraph) {
+    var s = paragraph.textContent;
+    if (s.trim().length > 0) {
+        var ss = [];
+        $(s.split(/([\s'’,\.\\-\\+\\*=\\(\\)\\:\\;\\"`]+)/g)).each(function () {
+            if (this.match(/[\s',\.\\-\\+\\*=\\(\\)\\:\\;\\"`]+/)) {
+                ss.push(document.createTextNode(this));
+            } else {
+                var span = document.createElement('span');
+                span.className = 'word';
+                span.innerText = this;
+                attachListeners(span);
+                ss.push(span);
+            }
+        });
+        $(paragraph).replaceWith($(ss));
+    }
+}
+
+chrome.extension.onRequest.addListener(function (request, sender, sendResponse) {
+    if (request.method == "getSelection")
+        console.log({data: window.getSelection().toString()});
+    else
+        console.log({});
+});
+
+function isInsideContentEditor(node) {
+    while (node = node.parentNode) {
+        if (node.contentEditable === "true") {
+            return true;
+        }
+    }
+    return false;
+}
+
+$(document).ready(function () {
+    setInterval(function () {
+        var walker = document.createTreeWalker(
+            document.body,
+            NodeFilter.SHOW_TEXT,
+            null,
+            false
+        );
+
+        var node;
+        var textNodes = [];
+        while (node = walker.nextNode()) {
+            if (node.textContent.length > 0 && !node.textContent.match(/^[\s'’,\.\\-\\+\\*=\\(\\)\\:\\;\\"`]+$/) &&
+                (node.parentNode.tagName !== 'SPAN' || node.parentNode.className !== "word") &&
+                    node.parentNode.tagName !== 'STYLE' && node.parentNode.tagName !== 'SCRIPT' &&
+                    node.parentNode.tagName !== 'TEXTAREA' &&
+                    node.parentNode.tagName !== 'INPUT' &&
+                node.parentNode.contentEditable !== "true" &&
+                !isInsideContentEditor(node) &&
+                getComputedStyle(node.parentNode)['white-space'] !== 'nowrap') {
+                //console.log('Found text node: \'' + node.textContent + '\'');
+                textNodes.push(node);
+            }
+        }
+
+        $(textNodes).each(function () {
+            spanify(this);
+        });
+    }, 2000);
+});
+
+function attachListeners(span) {
+    $(span).hover(function (e) {
+        clearTimeout(timer);
+        pos = getOffsetRect(this);
+        element = $(this);
+        timer = setTimeout(function () {
+            if (e.target.tagName == 'SPAN' && pressed) {
+                showPopup();
+            }
+        }, 0);
+    }, function (e) {
+        clearTimeout(timer);
+        timer = null;
+        hidePopup();
+    });
+}
+
+
+function getOffsetRect(elem) {
+    var box = elem.getBoundingClientRect();
+
+    var body = document.body;
+    var docElem = document.documentElement;
+
+    var scrollTop = window.pageYOffset || docElem.scrollTop || body.scrollTop;
+    var scrollLeft = window.pageXOffset || docElem.scrollLeft || body.scrollLeft;
+
+    var clientTop = docElem.clientTop || body.clientTop || 0;
+    var clientLeft = docElem.clientLeft || body.clientLeft || 0;
+
+    var top = box.top + scrollTop - clientTop;
+    var left = box.left + scrollLeft - clientLeft;
+
+    return { top: Math.round(top), left: Math.round(left) }
+}
+
+var timer;
+var pos;
+var element;
+
+var voiceTimer;
+function showPopup() {
+    translate(element.text(), 'en', 'ru', function (response) {
+        if (response.def && response.def[0] && response.def[0].ts && timer) {
+            $('.tooltip11').text(response.def[0].ts);
+            $('.tooltip11').css('left', pos.left + element.width() / 2 - $('.tooltip11').width() / 2 - 10)
+                .css('top', pos.top - 40);
+            $('.tooltip11_arrow').css('left', pos.left + element.width() / 2 - 10 + 4)
+                .css('top', pos.top - 40 + 20 + $('.tooltip11').height());
+            $('.tooltip11').show();
+            $('.tooltip11_arrow').show();
+            clearTimeout(voiceTimer);
+            voiceTimer = setTimeout(function () {
+                if (timer && pressed) {
+                    $('<iframe></iframe>').attr({
+                        src: 'https://translate.google.com/translate_tts?q=' + element.text() + '&tl=en_uk&sfsff=sfsf'
+                    }).hide().appendTo(document.body);
+                }
+            }, 2000);
+        }
+    })
+}
+
+function hidePopup() {
+    $('.tooltip11').hide();
+    $('.tooltip11_arrow').hide();
+}
+
+var pressed = false;
+$(document).bind('keydown', function (e) {
+    if (e.keyCode == 17 || e.keyCode == 91) {
+        pressed = true;
+        if (timer) {
+            showPopup();
+        }
+    }
+});
+$(document).bind('keyup', function (e) {
+    if (e.keyCode == 17 || e.keyCode == 91) {
+        pressed = false;
+        if (timer) {
+            hidePopup();
+        }
+    }
+});
+
+$('<div class="tooltip11">Tooltip</div><div class="tooltip11_arrow"></div>').appendTo(document.body);
+
+var cache = {};
+function translate(text, sl, tl, success) {
+    if (cache[text]) {
+        success(cache[text]);
+    } else {
+        $.ajax({
+            url: 'https://dictionary.yandex.net/api/v1/dicservice.json/lookup',
+            dataType: 'json',
+            data: {
+                key: 'dict.1.1.20140529T074913Z.e38dbea81d148538.6f3db42887e3685c11573236fa74642c446ba7af',
+                text: text,
+                lang: sl + '-' + tl
+            },
+            success: function (result) {
+                cache[text] = result;
+                success(result);
+            },
+            error: function (request, error, exception) {
+                alert(error);
+            }
+        });
+    }
+}
